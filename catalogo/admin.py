27344ea96b1bb2e1c7019dd7c7ec809894a5b5e9
davidvillegasvásquez <davidvillegas1@hotmail.com>
2026-06-05 16:13:25 -0400

    26065: se hizo useradmin en admin.py y se rectifico las etiquetas y campos para que django no buscara username, sino email. Se implemento un validador para no arrojar error de ejecutar Autor.objects.get(id=lista_ids[posicionRecibida])en la vista navDetailAutorYSusLibros cuando no hay autores y libros registrados.

---
+++ catalogo/admin.py
@@ -0,0 +1,60 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    # Agregamos los campos personalizados para poder visualizarlos y editarlos
+    model = CustomUser
+    #Se debe explicitar el orden, ya que usamos el usuario personalizado con email:
+    ordering = ('email',)
+    list_display = ['email', 'first_name', 'last_name', 'creador', 'is_staff']
+    search_fields = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'creador')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}), # Remove fields if your model doesn't have them
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'creador'), # Use your login identifier here
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# Register your models here.
+
+# Register your models here.
+from .models import Autor, Genero, Libro, LibroInstancia, Lenguaje
+
+# Define the admin class
+@admin.register(Autor)
+class AutorAdministrador(admin.ModelAdmin):
+    list_display = ('apellido', 'nombre', 'nacimiento', 'muerte')
+    #list_display_links = None (atributo que le quita el link al primer campo)
+    #fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+# Register the admin class with the associated model
+#admin.site.register(Autor, AutorAdministrador) 
+
+#Nota: cuando se usa el decorador @admin.register(modelo), no se usa la proposición de registro de asociación del modelo con la nueva clase personalizada, de lo contrario arroja el error de que ya ha sido registrado dicho modelo.
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Libro)
+class LibroAdministrador(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'mostrar_genero')
+
+@admin.register(LibroInstancia)
+class LibroInstanciaAdmin(admin.ModelAdmin):
+    list_display = ('libro', 'imprenta', 'id','estatus')
+    list_filter = ('estatus', 'debidoderegresar')
+    
+    fieldsets = ((None, {'fields': ('libro', 'imprenta', 'id')}),('Disponibilidad', {'fields': ('estatus', 'debidoderegresar', 'prestatario')}),)
+
+#Hacemos registros sencillos (no es necesario hacer una clase para ellos), para modelos sencillos de un sólo campo:
+
+admin.site.register(Genero)
+admin.site.register(Lenguaje)
--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,6 +1,57 @@
 from django.urls import path
 from . import views
+from django.contrib import admin #Aunque no se vé el uso del admin aquí, si no lo importo se presenta la excepción "LookupError: No installed app with label 'admin'".
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
+    path('', views.inicio, name='vistaHome'), 
+    path('libros/', views.LibroVistaLista.as_view(), name='todosLoslibros'),      path('libros/conbarbara', views.LibroVistaListaConBarbara.as_view(), name='librosConBarbara'), 
+    path('libro/<int:pk>', views.VistaDetalleLibro.as_view(), name='detallesDeLibro'), 
+    path('autores/', views.VistaListaGenAutores.as_view(), name='toditicosLosAutores'), 
+    path('autor/<int:pk>', views.VistaDetalladaGenAutor.as_view(), name='autorDetalles'),  
+    path('reseteoContSesiones', views.borrarConteoVisitas, name='resetearVisitas'),
+    path('LibrosEnManosDelUsuario/<str:username>', views.ListaLibrosPrestadosAlUsuario.as_view(), name='misLibrosalquilados'),
+    path('TodosLosLibrosActualmenteAlquilados/', views.ListaDeLibrosPrestadosActualmente.as_view(), name='librosAlquiladosActualmente'),
+    path('libro/<uuid:claveprimaria>/renovacion/', views.renovacionLibroPorLibrero, name='renovDeLibroPorLibrero'),
+    path('autor/crear/', views.CrearAutor.as_view(), name='crearautorini'),
+    path('autor/<int:pk>/actualizar/', views.ActualizarAutor.as_view(), name='actualizarEsteAutor'), path('autor/<int:pk>/borrar/', views.BorrarAutor.as_view(), name='borrar-autor'),
+    path('libro/crear/', views.CrearLibro.as_view(), name='crearlibro'),
+    path('libro/<int:pk>/actualizar/', views.ActualizarLibro.as_view(), name='actualizarLibro'), path('libro/<int:pk>/borrar/', views.BorrarLibro.as_view(), name='borrarlibro'),
+    path('ejemUsoSelectOptionsW3.css/', views.goTovistaGenDetailsAutorFromSelect, name='irAautorDetailsDesdeUnSelect'),
+    path('irAdetalleAutorDesdeFormulario/', views.irAdetalleAutorDesdeForm, name='autorDetailDesdeForm'),]
 
+#Ejemplos de cómo usar una función anónima lambda en lugar de usar vistas en views.py :
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
+urlpatterns += [path('vistaDeFuncionAnonimaParaRedirigirAunaUrl/', lambda solicitudQueNoDaraParametros: HttpResponseRedirect(reverse('todosLoslibros')), name='redirigiendoA-listaDelibros'),]
+
+#Note que podemos usar elementos html dentro de la pseudoplantilla que usamos en la función anónima lambda. Lo que no pudimo meter fue una etiqueta url django para ir a home: {% url 'vistaHome' %}
+urlpatterns += [path('UsandoUnHttpResponseSinPlantillaNiVista/', lambda solicitudcualquiera: HttpResponse("<p>-----Saludos!!!</p><a href='/catalogo/'>Ir a home</a>"), name='UsarUnHttpResponseSinPlantillaNiVista'),]
+
+urlpatterns += [
+    path('formChoiceFieldAutorYsusLibrosJS/', views.autorYsusLibrosChoiceFieldJS, name='formModelChoiceFieldAutorYsusLibrosJS'),
+    path('formChoiceFieldAutorYsusLibrosFormTools/', views.AutorYsusLibrosChoiceFielFormTools.as_view(), name='formToolsAutorYsusLibros'),
+    path('navAutorYsusLibrosConJS/', views.navDetailAutorYSusLibros, name='navAutorYsusLibJS'),
+    path('navAutorYsusLibrosConW3JS/', views.navDetailAutorYSusLibW3JS, name='navAutorYsusLibW3JS'),
+    path('navAutorYsusLibrosConW3JSauxiliar/', views.auxParaUsarW3jsIncludeHTMLEnAutorYsusLib, name='navAutorYsusLibW3JSauxiliar'),
+    path('navAutorYsusLibrosConHTMX/', views.navDetailAutorYSusLibHTMX, name='navAutorYsusLibHTMX'),
+    path('navAutoresConModelFormsetJS/', views.navAutorModelFormsetJS, name='navAutorModelFormsetJS'),
+    path('navAutoresConModelFormsetYpaginator/', views.navAutorModelFormsetYpaginator, name='navAutorModelFormsetYpaginator'),
+    path('navAutoresYsusLibrosConInlineformset/', views.navAutorYsusLibrosInlienformset, name='navAutorYsusLibrosInlineformset'),
+    path('todos-losTitulos-pdf/', views.descargar_pdf, name='descargar_pdf'),
+    #Endpoints de la api django rest framework:
+    path('libros/<int:pk>/', views.LibroDetalle.as_view(), name="libro-detail"),
+    path('libros/', views.Libros.as_view(), name="libro-list"),
+    path('autores/', views.Autores.as_view(), name="autor-list"),
+    path("api", views.api_root),
+    path("autores/<int:pk>/", views.AutorDetalle.as_view(), name="autor-detail"),
+    #Creación y actualización de usuarios:
+    #path('crear-usuario/', views.crear_usuario_inferior, name='crear_usuario'),
+    #path('usuarios/<str:miembros_de_la_libreria>/', views.UsuariosPorGrupoListView.as_view(), name='lista_usuarios_grupo'),
+    # Ruta para actualizar un usuario específico usando su primary key (pk)
+    #path('usuario/<int:pk>/editar/', views.UsuarioUpdateView.as_view(), name='editar_usuario'),
+    #path('usuario/<int:pk>/eliminar/', views.UsuarioDeleteView.as_view(), name='eliminar_usuario'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
