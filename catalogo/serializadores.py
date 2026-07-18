8f741645c399f7adcb0070a13756be029fc1c88b
davidvillegasvásquez <davidvillegas1@hotmail.com>
2026-07-18 02:29:24 -0400

    260717-2: Por fin logramos meter el campo genero en CrearLibro. Ahora queda pendiente como hacer múltiples selecciones en un control Dropdown para mandarlas como una lista de valores del campo genero. También un cuadro de diálogo que indique que se creó el libro o el ft.Text que dure unos segundo y se vaya a la pag de inicio (/).

--- catalogo/serializadores.py
+++ catalogo/serializadores.py
@@ -12,9 +12,12 @@ class SerializadorLibro(serializers.HyperlinkedModelSerializer):
     lenguaje = serializers.HyperlinkedRelatedField(
         many=False, view_name="lenguaje-detail", read_only=True
     )
-
+    #Así serializamos un campo con relación ManyToMany:
     genero = serializers.HyperlinkedRelatedField(
-        many=True, view_name="genero-list", read_only=True
+        many=True, 
+        view_name="genero-detail", 
+        read_only=True,
+        #queryset=Genero.objects.all()
     )
 
     class Meta:
@@ -34,9 +37,25 @@ class SerializadorLibro(serializers.HyperlinkedModelSerializer):
         extra_kwargs = {
             'url': {'view_name': 'libro-detail', 'format': 'json'}
         }
+    """
+    def create(self, validated_data):
+        # 1. Extraemos los campo de los datos validados
+        autor_data = validated_data.pop('autor_id', None)
+        lenguaje_data = validated_data.pop('lenguaje_id', None)
+        generos_data = validated_data.pop('genero_id', [])
 
+        # 2. Creamos la instancia del libro principal
+        libro = Libro.objects.create(**validated_data)
+
+        # 3. Guardamos/vinculamos las relaciones muchos a muchos manualmente
+        for gen in generos_data:
+            libro.genero.add(gen)
+
+        # 4. Retornamos la instancia creada
+        return libro        
+    """                
 class SerializadorAutor(serializers.HyperlinkedModelSerializer):
-    libros = serializers.HyperlinkedRelatedField(
+    libro = serializers.HyperlinkedRelatedField(
         many=True, view_name="libro-detail", read_only=True
     )
 
@@ -47,25 +66,26 @@ class SerializadorAutor(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Autor
-        fields = ["url", "id", "nombre", "apellido", "nacimiento", "muerte", "libros"]
+        fields = ["url", "id", "nombre", "apellido", "nacimiento", "muerte", "libro"]
         #Asegurarse de que el campo url apunte a la ruta correcta. Se usa para las operaciones c, u, and d de crud:
         extra_kwargs = {
             'url': {'view_name': 'autor-detail', 'format': 'json'}
         }
 
+
 class SerializadorGenero(serializers.HyperlinkedModelSerializer):
-    libros = serializers.HyperlinkedRelatedField(
+    libro = serializers.HyperlinkedRelatedField(
         many=True, view_name="libro-detail", read_only=True
     )
 
     class Meta:
         model = Genero
-        fields = ['url', 'id', 'nombre', 'libros']
+        fields = ['url', 'id', 'nombre', 'libro']
 
 class SerializadorLenguaje(serializers.HyperlinkedModelSerializer):
-    libros = serializers.HyperlinkedRelatedField(
+    libro = serializers.HyperlinkedRelatedField(
         many=True, view_name="libro-detail", read_only=True
     )
     class Meta:
         model = Lenguaje
-        fields = ['url', 'id', 'nombre', 'libros']
+        fields = ['url', 'id', 'nombre', 'libro']
